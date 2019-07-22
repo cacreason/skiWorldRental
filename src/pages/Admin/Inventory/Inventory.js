3de053a5130aba801347d5773119de1fd6ceb917
Chris Creason <chris@tigerfoxmedia.com>
2019-07-22 17:37:17 -0400

    Add search functionality on inventory page

--- src/pages/Admin/Inventory/Inventory.js
+++ src/pages/Admin/Inventory/Inventory.js
@@ -8,7 +8,6 @@ import AdminNav from '../../../components/AdminNav/AdminNav';
 import InventoryTable from '../../../components/InventoryTable/InventoryTable';
 import Pagination from '../../../components/Pagination/Pagination';
 import axios from 'axios';
-import queryString from 'query-string';
 import './styles.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
@@ -22,7 +21,8 @@ export default class AdminInventory extends React.Component {
       currentPage: 1,
       itemsPerPage: 25,
       totItems: 1,
-      data: this.fetchItemData()
+      data: "",
+      query: ""
     };
   }
   isOpenCallback = (dataFromChild) => {
@@ -39,17 +39,14 @@ export default class AdminInventory extends React.Component {
     this.setState({currentPage: page})
     console.log('page' + page);
   }
-  fetchItemData(){
-    const query = queryString.parse(this.props.location.search);
-    const params = new URLSearchParams(query);
-    const search = params.get('search');
-    axios.post("/admin/inventory/search", {"search" : search}, {
+
+  fetchItemData(query){
+    axios.post("/admin/inventory/search", {"search" : query}, {
       headers: {
           'Content-Type': 'application/json'
       }
     })
     .then((response) => {
-      console.log(response);
       if (response.status === 200){
         this.setState({data: response.data});
         }
@@ -61,8 +58,27 @@ export default class AdminInventory extends React.Component {
     });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let query = this.state.query;
+    let regex = /[\\><^&%*(){}[\]|]/gi;
+    let sanitizedQuery = query.replace(regex, "");
+    this.fetchItemData(sanitizedQuery);
+    console.log(sanitizedQuery);
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   componentDidMount(){
-    this.fetchItemData();
+    this.fetchItemData(this.state.query);
   }
 
   render() {
@@ -73,12 +89,12 @@ export default class AdminInventory extends React.Component {
       <Container fluid>
       <Row>
       <Col className="py-3">
-      <Form>
+      <Form method="post" onSubmit={this.handleSubmit}>
         <InputGroup className="search">
           <InputGroupAddon addonType="prepend" className="input-group-text"><FontAwesomeIcon icon="search" size="1x" className="my-auto mx-1"/></InputGroupAddon>
-          <Input placeholder="Item Description" size="30"/>
+          <Input placeholder="Item Description" size="30" name="query" onChange={this.handleInputChange}/>
           <InputGroupAddon addonType="append">
-          <Button outline color="primary" size="sm" className="px-2">Search</Button>
+          <Button outline color="primary" type="submit" size="sm" className="px-2">Search</Button>
           </InputGroupAddon>
         </InputGroup>
       </Form>
